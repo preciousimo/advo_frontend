409dd1a24b4a86c88268eb8d720b1f382b9eaea2
preciousimo <preciousimoniakemu@gmail.com>
2022-11-02 11:16:27 +0100

    Fetch data from API

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const HomePage = () => {
-  return (
-    <div>Home Page</div>
-  )
-}
+  const [advocates, setAdvocates] = useState([]);
 
-export default HomePage
+  useEffect(() => {
+    getData();
+  }, []);
+
+  let getData = async () => {
+    let response = await axios.get("http://127.0.0.1:8000/advocates/");
+    console.log("Response:", response);
+    setAdvocates(response.data)
+  };
+
+  return (
+    <div>
+        <h1>Home Page</h1>
+
+        <div>
+            {advocates.map((advocate, index) => (
+                <div key={index}>
+                    <strong>{advocate.username}</strong>
+                </div>
+            ))}
+        </div>
+    </div>
+  )
+};
+
+export default HomePage;
