85f8a328b3118f94827fff892133d2460ddb5302
preciousimo <preciousimoniakemu@gmail.com>
2022-11-02 01:19:51 +0100

    Create pages

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import HomePage from './pages/HomePage'
+import AdvocatePage from './pages/AdvocatePage'
 import './App.css';
 
 function App() {
---
+++ src/pages/AdvocatePage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const AdvocatePage = () => {
+  return (
+    <div>Advocate Page</div>
+  )
+}
