94a4e6210d92bfe22a8a8eaad444253d937312b8
preciousimo <preciousimoniakemu@gmail.com>
2022-11-03 12:03:32 +0100

    Display number of advocate found for search action

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,37 +4,41 @@ import axios from "axios";
 
 const HomePage = () => {
   const [advocates, setAdvocates] = useState([]);
-  const [total, setTotal] = useState(0)
-  const [pagination, setPagination] = useState(null)
+  const [total, setTotal] = useState(0);
+  const [pagination, setPagination] = useState(null);
 
   useEffect(() => {
-    getData(); 
+    getData();
   }, []);
 
-  let getData = async (query = '') => {
-    let response = await axios.get(`https://cados.up.railway.app/advocates?query=${query}`); 
+  let getData = async (query = "") => {
+    let response = await axios.get(
+      `https://cados.up.railway.app/advocates?query=${query}`
+    );
     setAdvocates(response.data.advocates);
-    setTotal(response.data.total)
-    setPagination(response.data.pagination)
+    setTotal(response.data.total);
+    setPagination(response.data.pagination);
   };
 
   let searchData = (e) => {
-    e.preventDefault()
-    let query = e.target.query.value
-    getData(query)
-  }
+    e.preventDefault();
+    let query = e.target.query.value;
+    getData(query);
+  };
 
   return (
     <div className="main_container">
       <h2>
-        Search {total} developer advocates found by @dennisivy's
-        webscraper and the TwitterAPI.
+        Search {total} developer advocates found by @preciousimo's webscraper
+        and the TwitterAPI.
       </h2>
+
+      <p>{pagination?.results_found} Devloper advocates found</p>
 
       <div className="form_wrapper">
         <form onSubmit={searchData} id="search_form">
-          <input  type="text" name="query" placeholder="Search advocates..." />
-          <input className="btn-primary"  type="submit" value="search"/>
+          <input type="text" name="query" placeholder="Search advocates..." />
+          <input className="btn-primary" type="submit" value="search" />
         </form>
       </div>
 
@@ -51,7 +55,9 @@ const HomePage = () => {
               <div>
                 <strong>{advocate.name}</strong>
                 <br />
-                <a href={advocate.twitter} target="_blank">@{advocate.username}</a>
+                <a href={advocate.twitter} target="_blank">
+                  @{advocate.username}
+                </a>
               </div>
             </div>
 
